cd92edc2129c5789d69b18e2f82f6d8b0b6e3f59
SatoshiKazuma <sairamkumarm21@gmail.com>
2022-07-02 09:35:43 +0530

    current bill calculation

---
+++ currentbill.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char username[20];
+    float charge, units;
+    printf("username: ");scanf("%s",username);
+    printf("Units: ");scanf("%f",&units);
+    printf("____________________________\n            BILL\n");
+
+}
