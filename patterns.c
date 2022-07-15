c6bee2af87515fb61c986939c0dbfe03a04e0da5
SatoshiKazuma <sairamkumarm21@gmail.com>
2022-07-15 17:44:26 +0530

    added left and right half pyramid

--- patterns.c
+++ patterns.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
-void main()
-{
-    for (int i = 1; i <= 5; i++)
-    {
-        for (int j = 1; j <= 5; j++)
-        {
-            if ((((j - 4) * (j - 2)) <= 0) && (((i - 4) * (i - 2)) <= 0))
-            {
-                printf("  ");
-            }
-            else
-            {
-                printf("* ");
-            }
-            
-        }
-        printf("\n");
-    }
-}
+// SQUARE WITH GAPS
+//  void main()
+//  {
+//      for (int i = 1; i <= 5; i++)
+//      {
+//          for (int j = 1; j <= 5; j++)
+//          {
+//              if ((((j - 4) * (j - 2)) <= 0) && (((i - 4) * (i - 2)) <= 0))
+//              {
+//                  printf("  ");
+//              }
+//              else
+//              {
+//                  printf("* ");
+//              }
+//          }
+//          printf("\n");
+//      }
+//  }
+
+//LEFT HALF PYRAMID
+// void main()
+// {
+//     for (int i = 0; i <= 5; i++)
+//     {
+//         for (int j = 0; j <= 5; j++)
+//         {
+//             if (j > 5 - i){
+//                 printf("* ");
+//             }
+//             else
+//             {
+//                 printf("  ");
+//             }
+//         }
+//         printf("\n");
+//     }
+// }
+
+//RIGHT HALF PYRAMID
+// void main(){
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+
