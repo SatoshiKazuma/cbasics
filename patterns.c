5f65080acb4b8d24b97e3091115f2e4d0a5ac3a9
SatoshiKazuma <sairamkumarm21@gmail.com>
2022-07-19 18:16:56 +0530

    black widow symbol added

--- patterns.c
+++ patterns.c
@@ -19,14 +19,45 @@
 //      }
 //  }
 
-//LEFT HALF PYRAMID
-// void main()
+// LEFT HALF PYRAMID
+//  void main()
+//  {
+//      for (int i = 0; i <= 5; i++)
+//      {
+//          for (int j = 0; j <= 5; j++)
+//          {
+//              if (j > 5 - i){
+//                  printf("* ");
+//              }
+//              else
+//              {
+//                  printf("  ");
+//              }
+//          }
+//          printf("\n");
+//      }
+//  }
+
+// RIGHT HALF PYRAMID
+//  void main(){
+//      for (int i = 1; i <= 5; i++)
+//      {
+//          for (int j = 1; j <= i; j++)
+//          {
+//              printf("* ");
+//          }
+//          printf("\n");
+//      }
+//  }
+
+// int main()
 // {
-//     for (int i = 0; i <= 5; i++)
+//     for (int i = 1; i <= 10; i++)
 //     {
-//         for (int j = 0; j <= 5; j++)
+//         for (int j = 1; j <= 10; j++)
 //         {
-//             if (j > 5 - i){
+//             if ((j == 1) || (j == 10) || (j == i) || (j == (11 - i)))
+//             {
 //                 printf("* ");
 //             }
 //             else
@@ -38,15 +69,15 @@
 //     }
 // }
 
-//RIGHT HALF PYRAMID
-// void main(){
-//     for (int i = 1; i <= 5; i++)
-//     {
-//         for (int j = 1; j <= i; j++)
-//         {
-//             printf("* ");
-//         }
-//         printf("\n");
-//     }
-// }
 
+// void main(){
+//     for (int i = 0; i <= 5; i++)
+//     {
+//         for (int j = 0; j <= 5; j++)
+//         {
+//             ;
+//         }
+        
+//     }
+    
+// }
